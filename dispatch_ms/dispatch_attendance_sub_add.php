b4427abe244482fa8108eff1df83c044255d7e87
WeiJu9755 <david.wu@i-powers.com.tw>
2025-10-17 16:26:42 +0800

    更新時鐘出不來問題

--- dispatch_ms/dispatch_attendance_sub_add.php
+++ dispatch_ms/dispatch_attendance_sub_add.php
@@ -217,6 +217,11 @@ EOT;
 
 
 $show_center=<<<EOT
+<link rel="stylesheet" type="text/css" href="/os/clockpicker-gh-pages/dist/jquery-clockpicker.css">
+<script type="text/javascript" src="/os/clockpicker-gh-pages/dist/jquery-clockpicker.js"></script>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 $style_css
 <div class="card card_full">
 	<div class="card-header text-bg-info">
